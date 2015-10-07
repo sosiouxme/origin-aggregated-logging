07034ed732d628b185192eae3068a8019ac0e9ba
Luke Meyer <lmeyer@redhat.com>
2015-10-07 14:19:37 -0400

    fix

--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -14,7 +14,7 @@ export CLUSTER_NAME=placeholder
 
 mkdir -p ${HOME}
 ln -s /usr/share/elasticsearch /usr/share/java/elasticsearch
-/usr/share/elasticsearch/bin/plugin -i io.fabric8.elasticsearch/openshift-elasticsearch-plugin/0.2
+/usr/share/elasticsearch/bin/plugin -i io.fabric8.elasticsearch/openshift-elasticsearch-plugin/0.3
 /usr/share/elasticsearch/bin/plugin -i io.fabric8/elasticsearch-cloud-kubernetes/1.3.0
 /usr/share/elasticsearch/bin/plugin -i com.floragunn/search-guard/0.5
 mkdir /elasticsearch
