79328b5bba501d5b4dc7eba253dfe3d8ad2cd1b8
Luke Meyer <lmeyer@redhat.com>
2015-10-08 08:50:07 -0400

    fix

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -63,9 +63,9 @@ mv $ES_CONF{.bak,}
 export ES_JAVA_OPTS="-Xbootclasspath/a:/usr/share/java/elasticsearch/plugins/search-guard/jackson-*.jar"
 # the amount of RAM allocated should be half of available instance RAM.
 # ref. https://www.elastic.co/guide/en/elasticsearch/guide/current/heap-sizing.html#_give_half_your_memory_to_lucene
-regex='^([[:numeric:]]+)([GgMm])$'
+regex='^([[:digit:]]+)([GgMm])$'
 if [[ "${INSTANCE_RAM}" =~ $regex ]]; then
-	ES_JAVA_OPTS="${ES_JAVA_OPTS} -Xmx$((${BASH_REMATCH[1]/2))${BASH_REMATCH[2]}"
+	ES_JAVA_OPTS="${ES_JAVA_OPTS} -Xmx$((${BASH_REMATCH[1]}/2))${BASH_REMATCH[2]}"
 else
 	echo "INSTANCE_RAM env var is invalid: ${INSTANCE_RAM}"
 	exit 1
