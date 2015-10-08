861d094a916e89ea569c054678b6493415ad4b81
Luke Meyer <lmeyer@redhat.com>
2015-10-08 17:45:05 -0400

    specify initial ES heap

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -70,7 +70,7 @@ export ES_JAVA_OPTS="-Xbootclasspath/p:/usr/share/java/elasticsearch/plugins/ope
 # ref. https://www.elastic.co/guide/en/elasticsearch/guide/current/heap-sizing.html#_give_half_your_memory_to_lucene
 regex='^([[:digit:]]+)([GgMm])$'
 if [[ "${INSTANCE_RAM}" =~ $regex ]]; then
-	ES_JAVA_OPTS="${ES_JAVA_OPTS} -Xmx$((${BASH_REMATCH[1]}/2))${BASH_REMATCH[2]}"
+	ES_JAVA_OPTS="${ES_JAVA_OPTS} -Xms256M -Xmx$((${BASH_REMATCH[1]}/2))${BASH_REMATCH[2]}"
 else
 	echo "INSTANCE_RAM env var is invalid: ${INSTANCE_RAM}"
 	exit 1
